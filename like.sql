0b51f9336db363572d2e4be03874b97d98442322
supriyasrinivas17 <supriyasrinivasbairi@gmail.com>
2025-07-26 12:34:05 +0530

    Add files via upload

---
+++ like.sql
@@ -0,0 +1,26 @@
+select*
+from parks_and_recreation.employee_salary;
+select*
+from employee_salary
+where occupation like 'D%'
+;
+select*
+from employee_salary
+where salary like '50%'
+;
+select*
+from employee_salary
+where first_name  like '%a%'
+;
+;
+select*
+from employee_salary
+where last_name  like '_e%'
+;
+;
+select*
+from employee_salary
+where salary like '_5%'
+;
+
+
