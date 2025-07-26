cb6cde0320f2512c914d95e9ba72d05285134039
supriyasrinivas17 <supriyasrinivasbairi@gmail.com>
2025-07-26 12:00:56 +0530

    Add files via upload

---
+++ select statements.sql
@@ -0,0 +1,16 @@
+select* 
+from parks_and_recreation.employee_demographics;
+select employee_id,
+first_name,
+last_name,
+age,
+gender
+from parks_and_recreation.employee_demographics;
+select first_name,
+age,
+age*10
+from parks_and_recreation.employee_demographics;
+select distinct age 
+from parks_and_recreation.employee_demographics;
+select distinct birth_date
+from parks_and_recreation.employee_demographics;
