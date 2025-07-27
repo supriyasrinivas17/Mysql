8c6d8da4aad332c52d4c7d3b495396bcab75e814
supriyasrinivas17 <supriyasrinivasbairi@gmail.com>
2025-07-27 12:20:58 +0530

    Add files via upload

---
+++ order by.sql
@@ -0,0 +1,12 @@
+select * 
+from employee_demographics
+order by age desc;
+select * 
+from employee_demographics
+order by age ;
+select * 
+from employee_demographics
+order by  birth_date;
+select * 
+from employee_demographics
+order by  birth_date, age desc;
