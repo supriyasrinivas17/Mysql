d355e90522b475fde8e6d9027506e76a274fb45c
supriyasrinivas17 <supriyasrinivasbairi@gmail.com>
2025-08-24 12:04:25 +0530

    Create trim.sql

---
+++ trim.sql
@@ -0,0 +1,7 @@
+select*from
+parks_and_recreation.employee_demographics;
+select*from 
+parks_and_recreation.employee_salary;
+select trim("                supppppp           ") as trimmed ;
+select rrim("                supppppp           ") as trimmed ;
+select trim("                supppppp           ") as trimmed ;
