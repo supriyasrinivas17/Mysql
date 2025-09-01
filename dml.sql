6e33fec517fdc627cc5ef1078369a955eea6f29b
supriyasrinivas17 <supriyasrinivasbairi@gmail.com>
2025-09-01 22:31:18 +0530

    Create dml.sql

---
+++ dml.sql
@@ -0,0 +1,6 @@
+insert table students values (stu_name,roll,section),("abc","700","a"),
+("ace","400","b");
+
+update students set roll = "7121" where section = "a";
+
+delete from students where roll = "7121";
