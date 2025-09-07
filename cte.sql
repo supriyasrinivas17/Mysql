09952b041994cb625d3881ae5bcb8480d5f9f575
supriyasrinivas17 <supriyasrinivasbairi@gmail.com>
2025-09-07 12:19:40 +0530

    Update cte.sql

--- cte.sql
+++ cte.sql
@@ -20,5 +20,34 @@ from employee_demographics as dem
   group by gender
   )
   select avg(max_salary) from cte_example;
+
+
+with cte_example as
+  (
+  select employee_id,gender,birth_date
+  from employee_demographics
+  where birth_date >'1985-01-01'
+  ),
+  cte_example2 as
+  (
+  select employee_id,salary
+  from employee_salary
+  where salary>50000
+  )
+  select*from 
+  cte_example as ct
+  join cte_example2 as ct2
+  on ct.employee_id = ct2.employee_id;
+  
+  
+  with cte_example (Gender,AVG_salary) as
+(
+select gender,avg(salary) over(partition by gender)
+from employee_demographics as dem
+  join employee_salary as sal
+  on dem.employee_id=sal.employee_id
+  )
+  select* from cte_example;
+
   
   
